7021b152cac03e05fd5ea01f443569a6e26565a7
DavidhlpaulaDev <128102444+Davidhlpaula25@users.noreply.github.com>
2025-12-17 12:20:13 -0300

    arrumando as variaveis da vecerl

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-// Substitua pelo SEU URL (copie do painel)
-const supabaseUrl = 'https://rzykakdivdyauwvwaumy.supabase.co'
-// Substitua pela SUA chave 'anon public' (copie do painel)
-const supabaseKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6eWtha2RpdmR5YXV3dndhdW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5NjY4MDUsImV4cCI6MjA4MTU0MjgwNX0.LgifJqcAVn_bsGNo-RM_3Z_mkzHavJTahXg83BcEuBA'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
